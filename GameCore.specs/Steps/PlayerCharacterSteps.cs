aaad3e9f71cd8d7cfad71c18bfbd2552aa1ae95a
Yoni Hirshowitz <jonhirsh39@gmail.com>
2023-05-16 14:47:44 -0400

    fixed project

---
+++ GameCore.specs/Steps/PlayerCharacterSteps.cs
@@ -0,0 +1,26 @@
+﻿using TechTalk.SpecFlow;
+
+namespace GameCore.Specs.Features
+{
+    [Binding]
+    public class PlayerCharacterSteps
+    {
+        [Given(@"I'm a new player")]
+        public void GivenIMANewPlayer()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I take 0 damage")]
+        public void WhenITake0Damage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"My health should now be 100")]
+        public void ThenMyHealthShouldNowBe100()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
